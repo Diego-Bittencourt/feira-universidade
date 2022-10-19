e50f3eff5704bb7238367019fa61d83058e7bcc7
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-10-20 01:10:54 +0900

    basic data displayed

---
+++ src/components/registro/EventoDados.vue
@@ -0,0 +1,32 @@
+<template>
+    <base-card>
+    <h1>Número de inscritos: {{inscritosList.length}}.</h1>
+    <h1>Número de inscritos presentes: {{checkedIn.length}}.</h1>
+    <h1>Número de inscritos que não fizeram o Check-in: {{notCheckedin.length}}</h1>
+    </base-card>
+</template>
+
+<script>
+export default {
+    computed: {
+        inscritosList() {
+            return this.$store.getters['signup/getInscritos'];
+        },
+        notCheckedin() {
+            const inscritosList = this.inscritosList;
+            const checkedIn = this.checkedIn;
+            const notCheckin = inscritosList.filter((element) => !checkedIn.includes(element.fullName))
+            return notCheckin;
+        },
+        checkedIn() {
+            return this.$store.getters['signup/getCheckins'];
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    margin: 1.5rem;
+}
+</style>
